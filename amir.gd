fe15fb7514aa0e344cb0cef1c647977e5e7f3498
HappyEevee0899 <114682624+HappyEevee0899@users.noreply.github.com>
2023-12-10 10:12:39 -0800

    gottem

--- amir.gd
+++ amir.gd
@@ -1,14 +1,17 @@
 extends CharacterBody2D
 @onready var character = $AnimatedSprite2D
-
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
 var currSpeed = 0.0
+var hasKite = false
+var crouch = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var jumpedCount = 0;
+
+var CorrectSound = preload("res://Rifle Shot Echo.mp3")
 
 var lastKey = false
 func _process(delta):
@@ -26,9 +29,16 @@ func _physics_process(delta):
 	else:
 		character.animation = "default"
 		
-
+	if (Input.is_action_pressed("ui_down")):
+		crouch = true
+	else:
+		crouch = false
+		
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		if hasKite and Input.is_action_pressed("ui_up") and velocity.y > 0:
+			velocity.y += gravity * delta / 10
+		else:
+			velocity.y += gravity * delta
 		character.animation = "prep"
 	
 	if is_on_floor():
@@ -36,18 +46,30 @@ func _physics_process(delta):
 
 	# Handle Jump.
 	if (Input.is_action_just_pressed("ui_up")) and jumpedCount < 2:
-		velocity.y = JUMP_VELOCITY
+		if crouch:
+			velocity.y = JUMP_VELOCITY / 2
+		else:
+			velocity.y = JUMP_VELOCITY
 		jumpedCount +=1
 	
+	if crouch:
+		character.animation = "crouch"
 	
+	if (Input.is_action_just_pressed("ui_text_backspace")):
+		hasKite = not hasKite
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = move_toward(velocity.x, direction * SPEED, SPEED)
+		if crouch:
+			velocity.x = move_toward(velocity.x, direction * SPEED / 2, SPEED / 2)
+		else:
+			velocity.x = move_toward(velocity.x, direction * SPEED, SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED * 2)
-
+		if crouch:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED * 2)
 
 	move_and_slide()
