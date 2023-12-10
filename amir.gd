e0a78da453922b163e63fc765cb611e0c15967c6
aravindkrishna2008 <aravindkrishna.nambi@gmail.com>
2023-12-10 09:15:11 -0800

    changed animations plus made node for level so we can toggle between

--- amir.gd
+++ amir.gd
@@ -5,7 +5,6 @@ const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
 var currSpeed = 0.0
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var jumpedCount = 0;
@@ -13,10 +12,13 @@ var jumpedCount = 0;
 var lastKey = false
 func _process(delta):
 	if (Input.is_action_just_pressed("ui_right")):
+		character.animation = "prep"
 		lastKey = false
 	elif (Input.is_action_just_pressed("ui_left")):
+		character.animation = "prep"
 		lastKey = true
-		
+	if (position.y > 1200):
+		get_tree().reload_current_scene()
 	character.flip_h = lastKey;
 
 func _physics_process(delta):
@@ -29,7 +31,7 @@ func _physics_process(delta):
 
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		character.animation = "prep"
+		character.animation = "jump"
 	
 	if is_on_floor():
 		jumpedCount = 0
