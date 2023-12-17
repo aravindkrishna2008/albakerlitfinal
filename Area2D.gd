f94d9532720d18ffd26f8ce6ea54485327ffcf0f
aravindkrishna2008 <86750995+aravindkrishna2008@users.noreply.github.com>
2023-12-16 17:59:53 -0800

    does it work boiz

--- Area2D.gd
+++ Area2D.gd
@@ -6,14 +6,18 @@ const JUMP_VELOCITY = -400.0
 var crouch = false
 #func _on_test_area_entered(body):
 	#queue_free()
+@onready var point_light_2d_2 = $PointLight2D2
 
 func _process(delta):
 	if phase == 0:
 		card.animation = "default"
 	#print(get_overlapping_bodies())
+	if len(get_overlapping_bodies()) > 0:
+		point_light_2d_2.energy = 0;
 	if len(get_overlapping_bodies()) > 0 and Input.is_action_just_pressed("ui_down") and phase == 0:
 		PlayerVariables.hasKite = true
 		card.animation = "text1"
+		position.y= position.y + 150
 		phase += 1
 		PlayerVariables.canMove = false
 		#queue_free()
---
+++ Card2.gd
@@ -0,0 +1,27 @@
+extends Area2D
+@onready var card = $AnimatedSprite2D2
+var phase = 0
+const SPEED = 200.0
+const JUMP_VELOCITY = -400.0
+var crouch = false
+#func _on_test_area_entered(body):
+	#queue_free()
+@onready var point_light_2d_2 = $PointLight2D2
+
+func _process(delta):
+	if phase == 0:
+		card.animation = "default"
+	#print(get_overlapping_bodies())
+	if len(get_overlapping_bodies()) > 0:
+		point_light_2d_2.energy = 0;
+	if len(get_overlapping_bodies()) > 0 and Input.is_action_just_pressed("ui_down") and phase == 0:
+		PlayerVariables.hasKite = true
+		card.animation = "text2"
+		position.y= position.y + 150
+		phase += 1
+		PlayerVariables.canMove = false
+		#queue_free()
+	elif phase == 1:
+		if Input.is_action_just_pressed("ui_down"):
+			queue_free()
+			PlayerVariables.canMove = true
